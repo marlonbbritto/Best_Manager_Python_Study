fdc77eb33b8487f56b83f6cdc18e3cd0476ac57d
marlonbbritto <marlonbbritto>
2024-03-07 23:07:57 -0300

    include the start settings

--- setup/settings.py
+++ setup/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'setup/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
